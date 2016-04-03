e6c6fbbce7f50bc1dba3a24eaa0c162234a6e12b
Alexandre Macedo <alexbmac@gmail.com>
2016-04-03 17:27:00 -0300

    evaluating part 1

--- src/Main.hs
+++ src/Main.hs
@@ -13,12 +13,15 @@ data LispVal = Atom String
              | Bool Bool
 
 
+instance Show LispVal where show = showVal
+
+
 spaces :: Parser ()
 spaces = skipMany1 space
 
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+=/:<=>?@^_~"
+symbol = oneOf "!#$%&|*+=-/:<=>?@^_~"
 
 
 parseString :: Parser LispVal
@@ -78,14 +81,62 @@ parseExpr = parseAtom
     <|> parseBothList
 
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
+
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                           if null parsed
+                               then 0
+                               else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func:args)) = apply func $ map eval args
 
 
 main :: IO ()
-main = do
-    (expr:_) <- getArgs
-    putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head
 
